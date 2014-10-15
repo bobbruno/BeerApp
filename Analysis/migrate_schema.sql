738c718e936e0987aad3ed4591d1b9c848e6e776
Roberto Bruno Martins <bobbruno@gmail.com>
2014-10-16 01:05:22 +0200

    Complementing the clustering info on repo

---
+++ Analysis/migrate_schema.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE features.tb_beer_style_percent1
+(
+   LIKE public.tb_beer_style_percent INCLUDING DEFAULTS INCLUDING CONSTRAINTS INCLUDING INDEXES INCLUDING STORAGE
+) 
+WITH (
+  OIDS = FALSE,
+  autovacuum_enabled = false
+)
+;
+ALTER TABLE features.tb_beer_style_percent1
+  OWNER TO "Django_beer";
+
+insert into features.tb_beer_style_percent1
+   select * from public.tb_beer_style_percent;
+
+select * from features.tb_user_style_percent1 where user_id = 144019 order by style_id
---
+++ Analysis/monitor_queries.sql
@@ -0,0 +1,3 @@
+﻿SELECT pid, query, query_start, waiting FROM pg_stat_activity where state = 'active' AND datname = 'BeerDb'
+
+
